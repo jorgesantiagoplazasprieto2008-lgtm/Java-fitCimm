37f4c6176bc1b062694e1bcee9d35095cd5f3486
Sagi123 <[JSPP33672483@soy.sena.edu.co]>
2026-07-22 12:39:38 -0500

    Refactor_Carpetas y ConexionDB actualizados

--- src/main/ConexionDB.java
+++ src/main/ConexionDB.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class conexion    {
+public class ConexionDB {
     private static final String URL    = "jdbc:sqlserver://localhost\\DESKTOP-15S3QIL\\SQLEXPRESS:1433;database=fitcimm";
 private static final String USUARIO = "IntelJ";
 private static final String CLAVE   = "123456";
