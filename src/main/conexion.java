091d3b71bdc1ce3d25eb30f4113c7c407ba6eb7b
Sagi123 <[JSPP33672483@soy.sena.edu.co]>
2026-07-21 23:42:24 -0500

    Primer commit

---
+++ src/main/conexion.java
@@ -0,0 +1,22 @@
+package main;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexion    {
+    private static final String URL    = "jdbc:sqlserver://localhost\\DESKTOP-15S3QIL\\SQLEXPRESS:1433;database=fitcimm";
+private static final String USUARIO = "IntelJ";
+private static final String CLAVE   = "123456";
+
+static {
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+        throw new RuntimeException("Driver MySQL no encontrado", e);
+    }
+}
+
+public static Connection getConexion() throws SQLException {
+    return DriverManager.getConnection(URL, USUARIO, CLAVE);
+}
+}
